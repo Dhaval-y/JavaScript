290ec65442ee8eee2a500536f624acd965adca0a
Dhaval-y <ujjwalvaish241304@gmail.com>
2025-07-06 09:25:43 +0000

    worked on Variable.js and Datatypes.js

---
+++ 01_Basics/variable.js
@@ -0,0 +1,6 @@
+const accountID = 1234567890
+let accountEmail = "ujjwal@gmail.com"
+var accountPass = "1234567890"
+let accountCity
+
+console.table([accountID, accountEmail, accountPass, accountCity])
