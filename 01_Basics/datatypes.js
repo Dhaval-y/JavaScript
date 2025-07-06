b850e7ab27806ef7b633634e42ab777c6e90a69f
Dhaval-y <ujjwalvaish241304@gmail.com>
2025-07-06 09:41:13 +0000

    Added Datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,14 @@
+let name = "Ujjwal"
+let age = 25
+let isLoggedIn = true
+let state = null
+let bigIntValue = 2343423435345234524356463456356563n
+let symbolValue = Symbol("This is a symbol") 
+
+console.table([name, age, isLoggedIn, state, bigIntValue, symbolValue])
+console.log(typeof name)  
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
+console.log(typeof bigIntValue)
+console.log(typeof symbolValue)
