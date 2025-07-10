ca57e47deefae5ea9138583c0c8c74c41625e299
Dhaval-y <ujjwalvaish241304@gmail.com>
2025-07-10 08:08:24 +0000

    learned typeconversion

---
+++ 01_Basics/typeconversion.js
@@ -0,0 +1,11 @@
+let score = 35
+console.log(typeof score)
+
+let number= Boolean(score)
+console.log(typeof number)
+console.log(number)
+
+let isLoggedIn = 1
+
+let isLoggedInBoolean = Boolean(isLoggedIn)
+console.log(isLoggedInBoolean)
