764d86308602e712ccc906103b18d3dee90c77b4
Dhaval-y <ujjwalvaish241304@gmail.com>
2025-07-30 07:38:32 +0000

    added functions.js

--- 02_Basics/array.js
+++ 02_Basics/array.js
@@ -2,4 +2,4 @@ const arr = [1, 2, 3, 4, 5];
 console.log(arr[0]); // Accessing the first element
 console.log(arr.concat(4));
 //console.log(arr.concat([6, 7, 8])); // Concatenating another array
-console.log(arr.concat(6, 7, 8)); 
+console.log(arr.concat(6, 7, 8));  
---
+++ 02_Basics/functions.js
@@ -0,0 +1,23 @@
+function sayMyName(){
+    console.log("U");
+    console.log("J");
+    console.log("J");
+    console.log("W");
+    console.log("A");
+    console.log("L");
+}
+
+//sayMyName(); 
+
+function addTwoNumbers(a, b) {
+    c = a + b;
+    console.log(c);
+}
+
+//addTwoNumbers(3,4)
+
+function loginUserMessage(username) {
+    return `${username} logged in successfully`
+}
+
+console.log(loginUserMessage("ujjwal"))
