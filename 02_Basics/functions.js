0c8b1ab08d5ba46638e085b3946479347730daab
Dhaval-y <ujjwalvaish241304@gmail.com>
2025-07-30 09:02:56 +0000

    Learned Functions.js

--- 02_Basics/functions.js
+++ 02_Basics/functions.js
@@ -20,4 +20,24 @@ function loginUserMessage(username) {
     return `${username} logged in successfully`
 }
 
-console.log(loginUserMessage("ujjwal"))
+//console.log(loginUserMessage("ujjwal"))
+
+const user = {
+    username: "Ujjwal",
+    price: 100,
+}
+
+function userInfo(anyobject){
+    console.log(`Username is ${anyobject.username}`);
+    console.log(`Price is ${anyobject.price}`);
+}
+
+userInfo(user); 
+
+const newArr = [200,400,600,800]
+
+function getSecondValue(arr){
+    return arr[1];
+}
+
+console.log(getSecondValue(newArr));
