648151b6a90219e7fa66cb326a189df49efde06a
Dhaval-y <ujjwalvaish241304@gmail.com>
2025-08-03 07:02:52 +0000

    Learned about switch-case in Javascript

---
+++ Control Flow/Two.js
@@ -0,0 +1,66 @@
+ const month = 5;
+
+ switch (month) {
+   case 1:
+     console.log("January");
+     break;
+   case 2:
+     console.log("February");
+     break;
+   case 3:
+     console.log("March");
+     break;
+   case 4:
+     console.log("April");
+     break;
+   case 5:
+     console.log("May");
+     break;
+   case 6:
+     console.log("June");
+     break;
+   case 7:
+     console.log("July");
+     break;
+   case 8:
+     console.log("August");
+     break;
+   case 9:
+     console.log("September");
+     break;
+   case 10:
+     console.log("October");
+     break;
+   case 11:
+     console.log("November");
+     break;
+   case 12:
+     console.log("December");
+     break;
+   default:
+     console.log("Invalid month number");
+ }
+
+ const maheena = "May";
+ switch (maheena) {
+   case "January":
+     console.log("It's the first month of the year");
+     break;
+   case "February":
+     console.log("It's the second month of the year");
+     break;
+   case "March":
+     console.log("It's the third month of the year");
+     break;
+   case "April":
+     console.log("It's the fourth month of the year");
+     break;
+   case "May":
+     console.log("It's the fifth month of the year");
+     break;
+   default:
+     console.log("Not a recognized month");
+ }
+
+ 
+ 
