d7f3cc48a60b20940ba502351459781c2242b934
Dhaval-y <ujjwalvaish241304@gmail.com>
2025-08-06 11:27:25 +0000

    few updates

--- hello_world_node/index.js
+++ hello_world_node/index.js
@@ -4,7 +4,7 @@ arr = [1, 2, 3, 4, 5];
 arr1 = [6, 7, 8, 9, 10];
 
 setTimeout(() => {
-
-    console.log(`[${arr + arr1}]` );
+    const  array =  arr + arr1
+    console.log(`Array is ${ array }`);
 }
 ,2000);
